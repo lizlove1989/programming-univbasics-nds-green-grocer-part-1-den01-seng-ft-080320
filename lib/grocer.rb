121215854a5907341d305b8126a5a6222eebdda3
Liz Love <love.elizabeth1989@gmail.com>
2020-07-15 23:19:02 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,14 +6,6 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart) 
-  puts cart[0][:count] = 1
-  # cart.map {|obj| 
-  #   if obj[:count]
-  #     obj[:count] += 1
-  #   else 
-  #     obj[:count] = 1
-  #   end
-  # }
 end
 
   
